92bcfc599e85aadbfbc960d36cfccd30636fac2d
Ethan Brierley <ethanboxx@gmail.com>
2019-06-04 11:15:27 +0100

    squash tiny bug

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ fetch('https://api.github.com/repos/El3m3nt4lIndustri3s/el3m3nt4lindustri3s.gith
             .then(res => res.json())
             .then(out => {
                 var commit = out[0].commit;
-                var commiter = commit.committer.name;
+                var commiter = commit.author.name;
                 var commit_msg = commit.message;
                 console.log('Output: ', commit);
                 document.getElementById("commit-last").innerHTML = "Committer: " + commiter + ". Message: " + commit_msg;
@@ -12,7 +12,7 @@ fetch('https://api.github.com/repos/El3m3nt4lIndustri3s/Marvel-Text-based-game/c
             .then(res => res.json())
             .then(out => {
                 var commit = out[0].commit;
-                var commiter = commit.committer.name;
+                var commiter = commit.author.name;
                 var commit_msg = commit.message;
                 console.log('Output: ', commiter);
                 document.getElementById("dcommit-lastt").innerHTML = "Committer: " + commiter + ". Message: " + commit_msg;
