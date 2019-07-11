e2a58fa565cbf9083a5c34ce688d53fa1cf04d1e
CorgigamingYT <43857110+CorgigamingYT@users.noreply.github.com>
2019-07-11 17:07:24 +0100

    javascript thing

--- script.js
+++ script.js
@@ -1,19 +1,13 @@
-fetch('https://api.github.com/repos/El3m3nt4lIndustri3s/el3m3nt4lindustri3s.github.io/commits')
-            .then(res => res.json())
-            .then(out => {
-                var commit = out[0].commit;
-                var commiter = commit.author.name;
-                var commit_msg = commit.message;
-                console.log('Output: ', commit);
-                document.getElementById("commit-last").innerHTML = "Committer: " + commiter + ". Message: " + commit_msg;
-            }).catch(err => console.error(err));
-
-fetch('https://api.github.com/repos/El3m3nt4lIndustri3s/Marvel-Text-based-game/commits')
-            .then(res => res.json())
-            .then(out => {
-                var commit = out[0].commit;
-                var commiter = commit.author.name;
-                var commit_msg = commit.message;
-                console.log('Output: ', commiter);
-                document.getElementById("dcommit-lastt").innerHTML = "Committer: " + commiter + ". Message: " + commit_msg;
-            }).catch(err => console.error(err));
+function commit_msg(repo, id) {
+    fetch('https://api.github.com/repos/El3m3nt4lIndustri3s/' + repo + '/commits')
+        .then(res => res.json())
+        .then(out => {
+            var commit = out[0].commit;
+            var commiter = commit.author.name;
+            var commit_msg = commit.message;
+            console.log('Output: ', commiter);
+            document.getElementById(id).innerHTML = "Committer: " + commiter + ". Message: " + commit_msg;
+        }).catch(err => console.error(err));
+}
+commit_msg("el3m3nt4lindustri3s.github.io", "commit-last");
+commit_msg("Marvel-Text-based-game", "dcommit-lastt");
